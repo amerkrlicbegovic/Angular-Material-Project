a9e79f0a12682294f5acc19a680193d6e3d638c9
Amer <amer.krlicbegovic@gmail.com>
2018-05-02 23:14:43 +0200

    Installing Angular Material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+})
+export class MaterialModule { }
