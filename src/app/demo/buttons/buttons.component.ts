87cf5c01adc6a5e6b5862109c6f2cdc1ec1ef16b
Amer <amer.krlicbegovic@gmail.com>
2018-05-03 00:34:31 +0200

    Creating a Demo Module

---
+++ src/app/demo/buttons/buttons.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  template: `
+  <button mat-button>
+  <mat-icon>face</mat-icon>
+  Click me
+</button>
+
+    <mat-checkbox></mat-checkbox><p>
+      buttons works!
+  `,
+  styles: []
+})
+export class ButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
