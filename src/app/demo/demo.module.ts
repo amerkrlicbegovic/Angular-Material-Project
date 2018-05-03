dea0b340d635ac8a3064c58adee933b588935fe6
Amer <amer.krlicbegovic@gmail.com>
2018-05-03 09:38:52 +0200

    Flexbox demo

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
-
-import { MaterialModule } from '../shared/material.module';
-import { FormsModule } from '@angular/forms';
 import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
@@ -13,6 +15,7 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
     CommonModule,
     DemoRoutingModule,
     MaterialModule,
+    FlexLayoutModule,
     FormsModule
   ],
   declarations: [ButtonsComponent, FlexboxComponent]
